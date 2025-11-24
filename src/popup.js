4a65bc74719bef8c030a83f9186b0c57607f00ba
christmaskid <chengyt2002@gmail.com>
2025-11-24 20:01:08 +0800

    Add host permissions and corresponding pop-up content

--- src/popup.js
+++ src/popup.js
@@ -13,5 +13,60 @@ function clickRadioButtonsWrapper() {
         });
     });
 }
-document.addEventListener('DOMContentLoaded', clickRadioButtonsWrapper);
-document.getElementById('clickRadios').addEventListener('click', clickRadioButtonsWrapper);
+
+// Check if current tab is a RedCap survey page
+function checkTabURL() {
+    return new Promise((resolve) => {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            let isRedcap = false;
+
+            var allow_host = "redcap.mc.ntu.edu.tw";
+            var allow_path_prefix = "/surveys/";
+            var show_div_name = "redcap-div";
+            var hide_div_name = "non-applicable-site";
+            
+            if (!tabs || !tabs[0] || !tabs[0].url) {
+                document.querySelector('.' + show_div_name).style.display = 'none';
+                document.querySelector('.' + hide_div_name).style.display = 'block';
+                console.log("No active tab or URL found.");
+                resolve(false);
+                return;
+            }
+
+            try {
+                const url = new URL(tabs[0].url);
+                console.log("Current tab URL:", url.hostname, url.pathname);
+
+                if (url.hostname === allow_host &&
+                    url.pathname.startsWith(allow_path_prefix)) {
+                    document.querySelector('.' + show_div_name).style.display = 'block';
+                    document.querySelector('.' + hide_div_name).style.display = 'none';
+                    console.log("This is a RedCap survey page.");
+                    isRedcap = true;
+                } else {
+                    document.querySelector('.' + show_div_name).style.display = 'none';
+                    document.querySelector('.' + hide_div_name).style.display = 'block';
+                    console.log("This is NOT a RedCap survey page.");
+                }
+            } catch (e) {
+                console.error(e);
+                document.querySelector('.' + show_div_name).style.display = 'none';
+                document.querySelector('.' + hide_div_name).style.display = 'block';
+                isRedcap = false;
+            }
+
+            resolve(isRedcap);
+        });
+    });
+}
+
+document.addEventListener('DOMContentLoaded', async function () {
+    document.getElementById('clickRadios')
+        .addEventListener('click', clickRadioButtonsWrapper);
+
+    const isRedcap = await checkTabURL();
+    console.log('checkTabURL result =', isRedcap);
+    if (isRedcap) {
+        clickRadioButtonsWrapper();
+    }
+});
